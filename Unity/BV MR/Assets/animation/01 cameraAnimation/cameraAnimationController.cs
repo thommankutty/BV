59ca84f9b8f603c1488d6b85b85455ac71575ada
Sree <sreeraj_ec@hotmail.com>
2018-03-24 16:52:07 +0800

    first Push

---
+++ Unity/BV MR/Assets/animation/01 cameraAnimation/cameraAnimationController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraAnimationController : MonoBehaviour {
+
+    //declare the animator
+    public Animator cameraAnim;
+
+	// Use this for initialization
+	void Start () {
+        // get the component
+        cameraAnim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("a"))
+        {
+            cameraAnim.Play("cameraAnimationOut");
+        };
+		
+	}
+}
---
+++ Unity/BV MR/Assets/animation/02 zAxisAnimation/zAxisAnimationController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zAxisAnimationController : MonoBehaviour {
+
+    public Animator armAnim;
+
+    // testing for sounds
+    public AudioClip musicClip;
+    public AudioSource musicSource;
+
+	// Use this for initialization
+	void Start () {
+
+        armAnim = GetComponent<Animator>();
+
+        // testing 
+        musicSource.clip = musicClip;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if (Input.GetKeyDown("z"))
+        {
+            armAnim.Play("zAxisAnimation_out");
+
+            // testing
+            musicSource.Play();
+          
+        };
+        
+	}
+}
